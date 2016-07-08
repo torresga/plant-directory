5abb285e4aae5933f2f26dc53f6f51709984d565
Gelsey Torres <gelsey.torres@gmail.com>
2016-07-08 18:11:09 -0400

    Adds new routes and views

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 var express = require("express");
-var app = express();
+var path = require("path");
+var routes = require("./routes/index");
 
-app.get('/', function(req, res) {
-  res.send("Hello World");
-});
+var app = express();
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use("/", routes);
+
+//app.get('/', function(req, res) {
+ // res.send("Hello World");
+//});
 
 app.use(express.static("public"));
 
 app.listen(3000, function(){
   console.log("Listening on port 3000");
 });
+
+module.exports = app;
