42670e54ade1c0dd9ee0b3f2cf90f43924ea8d45
Gelsey Torres <gelsey.torres@gmail.com>
2016-08-01 12:40:26 -0400

    Adds relationships to user model

--- app/models/user.js
+++ app/models/user.js
@@ -2,4 +2,7 @@ var db = require('../services/db.js');
 module.exports = db.Model.extend({
   tableName: 'users',
   hasTimestamps: true,
+  plants: function() {
+    return this.hasMany("Plant");
+  }
 }); 
