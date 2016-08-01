989d3c9a59d2db112f414415039dc6aa6c006676
Gelsey Torres <gelsey.torres@gmail.com>
2016-08-01 12:38:32 -0400

    Adds relationships to plant model

--- app/models/plant.js
+++ app/models/plant.js
@@ -1,6 +1,20 @@
 var db = require('../services/db');
+var User = require('./user');
 
 module.exports = db.Model.extend({
   tableName: 'plants',
   hasTimestamps: true,
+  user: function() {
+    return this.belongsTo(User);
+  },
+  toString: function() {
+    return [
+      "ID: ", this.id, ", ",
+      "name: ", this.attributes.name, ", ",
+      "common name: ", this.attributes.commonName, ", ",
+      "family: ", this.attributes.family, ", ",
+      "coordinates: ", this.attributes.coordinates, ", ",
+      "photoUrl: ", this.attributes.photoUrl, ", ",
+      "type: ", this.attributes.type, ", "
+    ].join("");
 });
